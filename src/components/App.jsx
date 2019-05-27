48d1bc2702b2904ae0033eb84beb447f8d35f0a8
khizerrehan <khizerrehan92@gmail.com>
2019-05-28 04:27:02 +0500

    cms: add new component and route for invalid path

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Home from './Home/Home.jsx';
 import About from './About/About.jsx';
 import Header from './common/Header.jsx'
+import PageNotFound from './PageNotFound.jsx'
 
 function App(){
     return (
         <div className="container-fluid">
             <Header />
-            <Route exact path="/" component={Home}></Route>
-            <Route path="/about" component={About}></Route>
+            <Switch>
+                <Route exact path="/" component={Home}></Route>
+                <Route path="/about" component={About}></Route>
+                <Route component={PageNotFound}></Route>
+            </Switch>
         </div>
     )
 }
---
+++ src/components/PageNotFound.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const PageNotFound = () => <h1><code>Oops!</code> Page Not Found</h1>
+
+export default PageNotFound;

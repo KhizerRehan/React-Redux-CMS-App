5ff31a4d28ae294a14a666677c9329e27da4ede8
khizerrehan <khizerrehan92@gmail.com>
2019-05-28 02:42:59 +0500

    cms: add header component for nav tabs

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import Home from './Home/Home.jsx';
 import About from './About/About.jsx';
+import Header from './common/Header.jsx'
 
 function App(){
     return (
         <div className="container-fluid">
+            <Header />
             <Route exact path="/" component={Home}></Route>
             <Route path="/about" component={About}></Route>
         </div>
---
+++ src/components/common/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {NavLink} from 'react-router-dom';
+
+const Header = () => {
+    const activeStyles = {
+        color: 'red'
+    };
+
+    return (
+        <nav>
+            <NavLink to="/" activeStyle={activeStyles} exact>Home</NavLink> {" | "}
+            <NavLink to="/about" activeStyle={activeStyles}>About</NavLink>
+        </nav>
+    )
+};
+
+export default Header;

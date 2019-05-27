a3ec513366ee6b3bcfbf1184b1c80062f8d6c7f0
khizerrehan <khizerrehan92@gmail.com>
2019-05-28 00:07:16 +0500

    cms: add home and about component

---
+++ src/components/About/About.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const About = () => (
+  <div>
+    <h2>About</h2>
+    <p>
+      This app uses React, Redux, React Router, and many other helpful
+      libraries.
+    </p>
+  </div>
+);
+
+export default About;

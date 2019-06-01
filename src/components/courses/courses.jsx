35ef24bc98344c3958348bcdf0c57757f8e842b7
khizerrehan <khizerrehan92@gmail.com>
2019-06-02 01:47:31 +0500

    cms: add add basic `add course` input form

--- src/components/courses/courses.jsx
+++ src/components/courses/courses.jsx
@@ -3,11 +3,39 @@ import React from 'react';
 class Courses extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            course:{
+                title:""
+            }
+        }
+    }
+
+    handleChange =  event =>{
+        const course = {...this.state.course, title:event.target.value};
+        this.setState({
+            course
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        alert(this.state.course.title);
     }
 
     render() {
         return (
-            <h2>Courses</h2>
+            <React.Fragment>
+                <form onSubmit={this.handleSubmit}>
+                    <h2>Courses</h2>
+                    <h3>Add course</h3>
+                    <input type="text"
+                    onChange = {this.handleChange}
+                    value={this.state.course.title}
+                    />
+
+                    <input type="submit" value="Save" />
+                </form>
+            </React.Fragment>
         )
     }
 }

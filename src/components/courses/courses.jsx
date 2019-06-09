da505778be74e76dbe28d986e9f6cb6b9935f58f
khizerrehan <khizerrehan92@gmail.com>
2019-06-10 00:43:45 +0500

    Revert "cms: remove `add form` component from course component"
    This reverts commit 922e13d5fff4738f48ed79d1142a96b6fc0a3caa.

--- src/components/courses/courses.jsx
+++ src/components/courses/courses.jsx
@@ -34,11 +34,33 @@ class Courses extends React.Component {
 
     }
 
+    handleChange =  event =>{
+        const course = {...this.state.course, title:event.target.value};
+        this.setState({
+            course
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.props.actions.createCourse(this.state.course);
+    }
 
     render() {
         return (
             <React.Fragment>
-                <CourseList courses={this.props.courses}/>             
+                <form onSubmit={this.handleSubmit}>
+                    <h2>Courses</h2>
+                    <h3>Add course</h3>
+                    <input type="text"
+                    onChange = {this.handleChange}
+                    value={this.state.course.title}
+                    />
+
+                    <input type="submit" value="Save" />
+
+                    <CourseList courses={this.props.courses}/>
+                </form>
             </React.Fragment>
         )
     }

171235880f8fc5ea23dfc82e2d46da424cb653b5
khizerrehan <khizerrehan92@gmail.com>
2019-06-10 00:06:49 +0500

    cms: add update course logic in  course reducer

--- src/redux/reducers/courseReducer.js
+++ src/redux/reducers/courseReducer.js
@@ -3,9 +3,13 @@ import initialState from './initialState';
 
 function courseReducer(state = initialState.courses, action) {
     switch(action.type) {
-        case types.CREATE_COURSE: 
+        case types.CREATE_COURSE_SUCCESS: 
          return [...state, {...action.course} ]; // (array of objects): add new course obj
 
+        case types.UPDATE_COURSE_SUCCESS: 
+          return state.map(course =>
+             course.id === action.course.id ? action.course : course);
+             
         case types.LOAD_COURSES_SUCCESS:
             return action.courses;
         default:
